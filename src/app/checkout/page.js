d0ecc06971f9a1addf3c697d07d47b5de80665a6
hariomp9 <hariompatil9@gmail.com>
2024-04-12 17:52:41 +0530

    form

---
+++ src/app/checkout/page.js
@@ -0,0 +1,333 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import logo from "../assets/logo.png";
+import sidemanu from "../../../public/images/side-menu.svg";
+import Footer from "../footer";
+import img from "./assets/butterpaneer3.png";
+import plus from "../../../public/images/plus.svg";
+import minus from "../../../public/images/minus.svg";
+
+const Checkout = () => {
+  return (
+    <>
+      <section>
+        <nav className="flex justify-center bg-[#F38181] 2xl:h-[116px] xl:h-[80px] lg:h-[50px] sm:h-[45px] h-12 ">
+          <div className="2xl:w-[1600px] xl:w-[1100px] lg:w-[850px]  md:w-[700px] w-full px-10 md:px-0  flex justify-between items-center">
+            <div className="w-1/3">
+              <Image
+                alt="image"
+                src={sidemanu}
+                className="2xl:w-[48px] 2xl:h-[40px] xl:w-[40px] xl:h-[25px] w-[30px] h-[20px]"
+              />
+            </div>
+            <div className="w-1/3 ">
+              <a href="#">
+                <Image alt="logo" src={logo} className="nav_logo" />
+              </a>
+            </div>
+            <div className="flex md:gap-7  gap-2">
+              <button className="nav_login">Log Inn</button>
+              <button className="nav_signup">Sign Up</button>
+            </div>
+          </div>
+        </nav>
+
+        <div className="flex justify-center bg-[#F5F5F5] 2xl:h-[90px] xl:h-[80px] lg:h-[50px] sm:h-[45px] h-12 ">
+          <div className="2xl:w-[1600px] xl:w-[1100px] lg:w-[850px]  md:w-[700px] w-full px-10 md:px-0  flex gap-[15px] items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="text-[#828282] w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M15.75 19.5 8.25 12l7.5-7.5"
+              />
+            </svg>
+            <Link href="/explore-dishes">
+              <button className="alata font-[400] text-[#828282] 2xl:text-[16px] 2xl:leading-[20px] xl:text-[12px] xl:leading-[18px] text-[10px] leading-[16px]">
+                Back to Chef’s Menu
+              </button>
+            </Link>
+          </div>
+        </div>
+
+        <div>
+          <div className="2xl:w-[1600px] xl:w-[1100px] lg:w-[850px]  md:w-[700px] w-full  md:px-0 items-center 2xl:px-[75px] xl:px-[50px] px-[35px] mx-auto">
+            <h1 className="pop-head 2xl:my-[30px]">Delivery information</h1>
+            <div className="flex justify-between 2xl:gap-[55px] xl:gap-[40px] gap-[25px] 2xl:mb-[110px]">
+              <form className="2xl:w-[795px] xl:w-[595px] w-[395px]">
+                <div>
+                  <label className="checkoutlable">
+                    Phone <span className="text-[#DB1414]">*</span>
+                  </label>
+                  <input
+                    placeholder="Enter phone number"
+                    className="w-full bg-[#F3F3F3] h-[60px] p-[20px] mt-[10px]"
+                  />
+                </div>
+                <div>
+                  <div className="my-[20px]">
+                    <label className="checkoutlable ">
+                      Shipping Address <span className="text-[#DB1414]">*</span>
+                    </label>
+                  </div>
+                  <div className="form-control flex flex-row gap-5 items-center">
+                    <label className="label cursor-pointer w-[15px] h-[15px]">
+                      <input
+                        type="checkbox"
+                        defaultChecked
+                        className="checkbox w-[15px] h-[15px] bg-[#256EFA]"
+                      />
+                    </label>
+                    <span className="label-text alata font-[400]  2xl:text-[16px] 2xl:leading-[26px] xl:text-[12px] xl:leading-[18px] text-[10px] leading-[16px]">
+                      Add a new address
+                    </span>
+                  </div>
+                </div>
+                <div className="flex justify-between 2xl:gap-[20px] 2xl:my-[15px]">
+                  <div className="2xl:w-[388px] xl:w-[288px] w-[188px]">
+                    <label className="checkoutlable">
+                      House No. <span className="text-[#DB1414]">*</span>
+                    </label>
+                    <input
+                      placeholder="Enter phone number"
+                      className="w-full bg-[#F3F3F3] h-[60px] p-[20px] mt-[10px]"
+                    />
+                  </div>
+                  <div className="2xl:w-[388px] xl:w-[288px] w-[188px]">
+                    <label className="checkoutlable">
+                      House No. <span className="text-[#DB1414]">*</span>
+                    </label>
+                    <input
+                      placeholder="Enter phone number"
+                      className="w-full bg-[#F3F3F3] h-[60px] p-[20px] mt-[10px]"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <label className="checkoutlable">
+                    Street Name <span className="text-[#DB1414]">*</span>
+                  </label>
+                  <input
+                    placeholder="Enter phone number"
+                    className="w-full bg-[#F3F3F3] h-[60px] p-[20px] mt-[10px]"
+                  />
+                </div>
+                <div className="flex justify-between 2xl:gap-[20px] 2xl:my-[15px]">
+                  <div className="2xl:w-[251px] xl:w-[200px] w-[140px]">
+                    <label className="checkoutlable">
+                      Town/City <span className="text-[#DB1414]">*</span>
+                    </label>
+                    <input
+                      placeholder="Enter phone number"
+                      className="w-full bg-[#F3F3F3] h-[60px] p-[20px] mt-[10px]"
+                    />
+                  </div>
+                  <div className="2xl:w-[251px] xl:w-[200px] w-[140px]">
+                    <label className="checkoutlable">
+                      County <span className="text-[#DB1414]">*</span>
+                    </label>
+                    <input
+                      placeholder="Enter phone number"
+                      className="w-full bg-[#F3F3F3] h-[60px] p-[20px] mt-[10px]"
+                    />
+                  </div>
+                  <div className="2xl:w-[251px] xl:w-[200px] w-[140px]">
+                    <label className="checkoutlable">
+                      Postcode <span className="text-[#DB1414]">*</span>
+                    </label>
+                    <input
+                      placeholder="Enter phone number"
+                      className="w-full bg-[#F3F3F3] h-[60px] p-[20px] mt-[10px]"
+                    />
+                  </div>
+                </div>
+                <div className="flex justify-between 2xl:gap-[20px] 2xl:my-[15px]">
+                  <div className="2xl:w-[388px] xl:w-[288px] w-[188px]">
+                    <label className="checkoutlable">
+                      First Name <span className="text-[#DB1414]">*</span>
+                    </label>
+                    <input
+                      placeholder="Enter phone number"
+                      className="w-full bg-[#F3F3F3] h-[60px] p-[20px] mt-[10px]"
+                    />
+                  </div>
+                  <div className="2xl:w-[388px] xl:w-[288px] w-[188px]">
+                    <label className="checkoutlable">
+                      Last Name <span className="text-[#DB1414]">*</span>
+                    </label>
+                    <input
+                      placeholder="Enter phone number"
+                      className="w-full bg-[#F3F3F3] h-[60px] p-[20px] mt-[10px]"
+                    />
+                  </div>
+                </div>
+                <hr className="my-[50px]" />
+                <div>
+                  <h1 className="pop-head 2xl:my-[5px]">Payment Details</h1>
+                  <p className="checkoutlable text-[#929292]">
+                    Shop securely and conveniently. All transactions are
+                    encrypted
+                  </p>
+                </div>
+                <div className="form-control flex flex-row gap-5 items-center my-[30px]">
+                  <label className="label cursor-pointer w-[15px] h-[15px]">
+                    <input
+                      type="checkbox"
+                      defaultChecked
+                      className="checkbox w-[15px] h-[15px] bg-[#256EFA]"
+                    />
+                  </label>
+                  <span className="label-text alata font-[400]  2xl:text-[16px] 2xl:leading-[26px] xl:text-[12px] xl:leading-[18px] text-[10px] leading-[16px]">
+                    New Credit Card
+                  </span>
+                </div>
+                <div className="flex 2xl:gap-[20px] 2xl:my-[15px]">
+                  <div className="2xl:w-[281px] xl:w-[200px] w-[148px]">
+                    <label className="checkoutlable">
+                      Card details <span className="text-[#DB1414]">*</span>
+                    </label>
+                    <input
+                      placeholder="Enter phone number"
+                      className="w-full bg-[#F3F3F3] h-[60px] p-[20px] mt-[10px]"
+                    />
+                  </div>
+                  <div className="2xl:w-[100px] xl:w-[288px] w-[188px]">
+                    <label className="checkoutlable">
+                      CVV<span className="text-[#DB1414]">*</span>
+                    </label>
+                    <input
+                      placeholder="Enter phone number"
+                      className="w-full bg-[#F3F3F3] h-[60px] p-[20px] mt-[10px]"
+                    />
+                  </div>
+                </div>
+                <div className="flex 2xl:gap-[20px] 2xl:my-[15px]">
+                  <div className="2xl:w-[149px] xl:w-[200px] w-[148px]">
+                    <label className="checkoutlable">
+                      Expiration <span className="text-[#DB1414]">*</span>
+                    </label>
+                    <input
+                      placeholder="Enter phone number"
+                      className="w-full bg-[#F3F3F3] h-[60px] p-[20px] mt-[10px]"
+                    />
+                  </div>
+                  <div className="2xl:w-[100px] xl:w-[288px] w-[188px]">
+                    <label className="checkoutlable">
+                      Zip code<span className="text-[#DB1414]">*</span>
+                    </label>
+                    <input
+                      placeholder="Enter phone number"
+                      className="w-full bg-[#F3F3F3] h-[60px] p-[20px] mt-[10px]"
+                    />
+                  </div>
+                </div>
+                <div className="form-control my-[35px]">
+                  <label className="flex gap-5 items-center ">
+                    <span className="label-text">Save card</span>
+                    <input
+                      type="checkbox"
+                      defaultChecked
+                      className="checkbox checkbox-info rounded-none w-[18px] h-[18px]"
+                    />
+                  </label>
+                </div>
+              </form>
+
+              <div>
+                <div className="2xl:w-[597px] xl:w-[395px] w-[295px] p-5 border 2xl:mt-[35px] max-h-[963px]">
+                  <div className="">
+                    {/* {cart.map((item) => ( */}
+                    <div>
+                      <div className="flex justify-between items-center 2xl:my-6 my-2">
+                        <div className="flex items-center gap-2 2xl:gap-4 xl:h-[70px]">
+                          <Image
+                            src={img}
+                            // alt={item.name}
+                            className="2xl:w-[83px]  xl:w-[65px] lg:w-[50px] rounded-[10px]"
+                          />
+                          <div>
+                            <h1 className="alata font-[400] text-[#111] 2xl:my-0 2xl:text-[24px] 2xl:leading-[34px]  xl:text-[12px] xl:leading-[20px] lg:text-[10px] lg:leading-[18px]">
+                              {/* {item?.title}
+                               */}
+                              Bhindi Sabjhi
+                            </h1>
+                            <h1 className="alata font-[400] text-[#111] 2xl:my-0 2xl:text-[20px] 2xl:leading-[28px]  xl:text-[14px] xl:leading-[20px] lg:text-[10px] lg:leading-[18px]">
+                              {/* £{item?.price} */}
+                              £8.50
+                            </h1>
+                          </div>
+                        </div>
+                        <div className="flex justify-center 2xl:w-[103px] 2xl:h-[39px] xl:w-[60px] xl:h-[22px] lg:w-[50px] lg:h-[20px] border rounded-[5px] ">
+                          {" "}
+                          <button
+                            className="   text-[#DB5353] rounded-l w-1/3"
+                            // onClick={() => {
+                            //   handleDecrement(item?.id);
+                            //   removeFromCart(item.id);
+                            //   alert("Removed from cart");
+                            // }}
+                          >
+                            <Image
+                              src={minus}
+                              className="2xl:w-[15px] 2xl:h-[15px] xl:w-[10px] xl:h-[10px] lg:w-[8px] lg:h-[8px] mx-auto "
+                            />
+                          </button>
+                          <p className=" flex mx-auto items-center text-[10px] xl:text-[12px] 2xl:text-[18px]  2xl:leading-[28px] ">
+                            {/* {count} */}
+                          </p>
+                          <button
+                            className="    text-[#DB5353] rounded-r w-1/3"
+                            // onClick={() => handleIncrement(item?.id)}
+                          >
+                            <Image
+                              src={plus}
+                              className="2xl:w-[15px] 2xl:h-[15px] xl:w-[10px] xl:h-[10px] lg:w-[8px] lg:h-[8px] mx-auto "
+                            />
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                    {/* ))} */}
+                    <div className="flex justify-between">
+                      <h1 className="alata font-[400] text-[#555555] 2xl:my-0 2xl:text-[18px] 2xl:leading-[28px]  xl:text-[14px] xl:leading-[20px] lg:text-[10px] lg:leading-[18px]">
+                        Subtotal
+                      </h1>
+
+                      <h1 className="alata font-[400] text-[#555555] 2xl:my-0 2xl:text-[18px] 2xl:leading-[28px]  xl:text-[14px] xl:leading-[20px] lg:text-[10px] lg:leading-[18px]">
+                        {/* £{item?.price} */}
+                        £8.50
+                      </h1>
+                    </div>
+                    <div className="flex justify-between items-center mt-20">
+                      <div>
+                        <h1 className="alata font-[400] text-[#111] 2xl:my-0 2xl:text-[18px] 2xl:leading-[28px] xl:text-[12px] xl:leading-[20px] lg:text-[10px] lg:leading-[18px]">
+                          {/* {subtotalPrice} */}
+                        </h1>
+                      </div>
+                      <div>
+                        {/* <button className=" alata font-[400] bg-[#DB5353] text-white mx-auto rounded-[5px] 2xl:w-[164px] 2xl:h-[56px] 2xl:text-[20px] 2xl:leading-[27.6px] xl:text-[12px] lg:text-[10px] xl:px-6 xl:py-[10px] lg:px-3 lg:py-1 px-3 py-1 ">
+                        Checkout
+                      </button> */}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <Footer />
+      </section>
+    </>
+  );
+};
+
+export default Checkout;
--- src/app/chef-details/page.js
+++ src/app/chef-details/page.js
@@ -1,18 +1,16 @@
 import dynamic from "next/dynamic";
-import React from 'react'
-import Image from 'next/image';
-import Link from 'next/link';
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 import logo from "../assets/logo.png";
 import sidemanu from "../../../public/images/side-menu.svg";
-import fb from "../../../public/images/fb.svg";
-import insta from "../../../public/images/insta.svg";
-import tiktok from "../../../public/images/tiktok.svg";
+import Footer from "../footer";
 
 const ChefDetail = () => {
   return (
     <>
       <section>
-      <nav className="flex justify-center bg-[#F38181] 2xl:h-[116px] xl:h-[80px] lg:h-[50px] sm:h-[45px] h-12 ">
+        <nav className="flex justify-center bg-[#F38181] 2xl:h-[116px] xl:h-[80px] lg:h-[50px] sm:h-[45px] h-12 ">
           <div className="2xl:w-[1600px] xl:w-[1100px] lg:w-[850px]  md:w-[700px] w-full px-10 md:px-0  flex justify-between items-center">
             <div className="w-1/3">
               <Image
@@ -33,98 +31,10 @@ const ChefDetail = () => {
           </div>
         </nav>
 
-        <div>
-
-        </div>
-
-        <footer className="bg-[#F6F6F6] flex justify-center pt-5 lg:pt-8 2xl:pt-14">
-          <div className="2xl:w-[1600px] xl:w-[1100px] lg:w-[850px] md:w-[700px] w-full px-10 md:px-0 mx-auto  2xl:mt-[100px] xl:mt-[50px] lg:mt-[35px]">
-            <div className="flex justify-between">
-              <div className="">
-                <div>
-                  <h1 className="footer_heading">Quick Links</h1>
-                  <p className="footer_text">Our Story</p>
-                  <p className="footer_text">Food Safety</p>
-                  <p className="footer_text">Help Center</p>
-                  <p className="footer_text">Global Cuisines</p>
-                </div>
-              </div>
-              <div className="">
-                <div>
-                  <h1 className="footer_heading">Resources</h1>
-                  <p className="footer_text">
-                    Become a chef
-                    <p className="footer_text">Browse more chef</p>
-                    <p className="footer_text">Homemade food delivery</p>
-                  </p>
-                </div>
-              </div>
-              <div className="">
-                <div>
-                  <h1 className="footer_heading">Other Links</h1>
-                  <p className="footer_text">Log In</p>
-                  <p className="footer_text">Sign Up</p>
-                  <p className="footer_text">Privacy Policy</p>
-                  <p className="footer_text">Terms of Service</p>
-                </div>
-              </div>
-              <div className="">
-                <div>
-                  <h1 className="footer_heading">Connect with us</h1>
-                  <div className="flex gap-2 justify-center lg:justify-start">
-                    <div>
-                      <Link
-                        href="https://www.facebook.com/profile.php?id=61553576243338"
-                        target="_blank"
-                      >
-                        <Image
-                          alt="image"
-                          src={fb}
-                          className="2xl:w-[30px] 2xl:h-[30px] h-auto w-auto"
-                        />
-                      </Link>
-                    </div>
-
-                    <div>
-                      <Link
-                        href="https://www.instagram.com/authentichef"
-                        target="_blank"
-                      >
-                        <Image
-                          alt="image"
-                          src={insta}
-                          className="2xl:w-[30px] 2xl:h-[30px] h-auto w-auto"
-                        />
-                      </Link>
-                    </div>
-                    <div>
-                      <Link
-                        href="https://www.tiktok.com/@authentichef"
-                        target="_blank"
-                      >
-                        <Image
-                          alt="image"
-                          src={tiktok}
-                          className="2xl:w-[30px] 2xl:h-[30px] h-auto w-auto"
-                        />
-                      </Link>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <hr className="2xl:mt-[56px] xl:mt-[25px] lg:mt-[15px] mt-2" />
-            <div>
-              <div className="flex justify-center 2xl:my-5 xl:my-3 lg:my-2">
-                <h1 className="footer_text_b">
-                  © 2024 Authentichef | All Rights Reserved
-                </h1>
-              </div>
-            </div>
-          </div>
-        </footer>
+   
+        <Footer />
       </section>
     </>
-  )
-}
+  );
+};
 export default dynamic(() => Promise.resolve(ChefDetail), { ssr: false });
